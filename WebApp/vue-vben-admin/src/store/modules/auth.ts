1291d5320c911638095d23805f90dd9e2dce89e5
guili95 <1539720748@qq.com>
2022-12-30 10:31:38 +0800

    修改前端授权配置

--- WebApp/vue-vben-admin/src/store/modules/auth.ts
+++ WebApp/vue-vben-admin/src/store/modules/auth.ts
@@ -8,7 +8,7 @@ import { TOKEN_KEY } from '/@/enums/cacheEnum';
 const host = location.origin;
 
 const userManager: UserManager = new Oidc.UserManager({
-  authority: 'http://192.168.0.13',
+  authority: 'https://auth.threebody.shop',
   client_id: 'ABPMicroServiceProAdmin',
   redirect_uri: encodeURI(`${host}/#/oidcSignIn`),
   response_type: 'code',
